944d17b5cd102e02c170c9d571f639f7deb05961
1303144162 <1303144162@qq.com>
2018-03-28 18:35:31 +0800

    new 1

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<title>门户</title>
+<link href="css/css.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div class="topbox navbar">
+<div class="container">
+<ul><li>首页</li><li>资源</li>
+<li>娱乐娱乐</li>
+<li style="clear:both; padding:0;"></li>
+</ul>
+
+</div>
+
+</div>
+
+
+
+</body>
+</html>
